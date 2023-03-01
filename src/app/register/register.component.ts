35efe7d88395c843a66ed918c175a5a87549c167
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-03-01 23:54:53 +0530

    displaying all students data

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../models/student.model';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  students:Student[]=[];
+
+   constructor(private userservice:UserService){}
+
+   ngOnInit() : void{
+    this. getallStudents();
+   }
+
+   getallStudents(){
+    this.userservice.getAllStudents().subscribe(res=>
+      {
+       this.students=res;
+       console.log(this.students);
+      });
+   }
+   
 }

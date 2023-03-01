1f30142dd0197ca22c0e77e6293181eb19bdf378
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-03-01 23:53:59 +0530

    with observables creating getAllStudents data

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import{Observable} from 'rxjs';
+import { Student } from './models/student.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private baseUrl ="https://localhost:7068/api/";
+  private baseUrl ="https://localhost:7068/api/Students";
   constructor(private http:HttpClient) { }
 
   
- getAllStudents(){
-  this.http.get(this.baseUrl).subscribe((
-    resultData:any)=>
-    {
-      
-    });
+ getAllStudents() : Observable<Student[]>  {
+  return this.http.get<Student[]>(this.baseUrl);
  }
 
 }

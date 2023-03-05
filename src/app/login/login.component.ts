0c0890fd9042e0bdaf136b1499ca8b1bb27be556
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-03-05 21:59:50 +0530

    importing service compoennts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Student } from '../models/student.model';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+ 
+    UserIsValid:boolean=false;
+    http: any;
+username: any;
+pass: any;
 
+   constructor(private userservice:UserService){}
+
+   loginSubmitted(){
+
+   }
+  
+  
 }

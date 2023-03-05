6df53736a325c7286e0f0168f0b16d3e2aa57c7d
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-03-06 00:01:32 +0530

    viewdetails path added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ParentComponent } from './parent/parent.component';
+import { ViewdetailsComponent } from './viewdetails/viewdetails.component';
 
 const routes: Routes = [
   // {path:'',component:ParentComponent},
@@ -12,9 +13,10 @@ const routes: Routes = [
       children:[
         {path:'parent/login',component:LoginComponent},
         {path:'parent/register',component:RegisterComponent}
-      ]}
+      ]},
   // {path:'login',component:LoginComponent},
   // {path:'register',component:RegisterComponent}
+  {path:'viewdetails/:id',component:ViewdetailsComponent}
 ];
 
 @NgModule({

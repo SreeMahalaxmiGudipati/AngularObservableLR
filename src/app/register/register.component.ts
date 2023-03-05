70ff65b1e1764f1efa4cee89587ba8106cf0a8c0
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-03-05 22:16:19 +0530

    FromGroup added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -13,38 +13,69 @@ export class RegisterComponent implements OnInit {
 
   student = new Student();
   students=[];
-  currentStudentID=[];
-
-  route: any;
-  activeindex: any;
+  displayMsg:string='';
+  isAccountCreated: boolean=false;
   data: any;
-
+  
 
    constructor(private userservice:UserService){
-    
+    this.getData();
    }
+
+   public registerForm =new FormGroup({
+    Name:new FormControl(''),
+    Phone:new FormControl(),
+    pass:new FormControl('')
+  });
+    
+  ngOnInit() : void{
+    this.getData();
+    }
+
+  registerSubmitted(){
+      
+              this.userservice.registerUser([
+                this.registerForm.value.Name,
+                this.registerForm.value.Phone,
+                this.registerForm.value.pass
+            ]).subscribe((res: any)=>{
+              if(res==''){
+                this.displayMsg='Something wrong';
+                console.log(res);
+                this.isAccountCreated=true;
+                this.getData();
+              }
+              else {
+              
+                this.displayMsg='Account created successfully';
+                console.log(res);
+                this.isAccountCreated=false;
+                this.getData();
+              }
+      });
+   }
+
+  
 
   getData()
   {
     this.userservice.getAllStudents().subscribe((data: any)=>
     {
       this.data=data;
-      console.log(this.data);
+      // console.log(this.data);
     })
   }
    
   DeleteStudentinfo(id:any){
     this.userservice.DeleteStudent(id).subscribe((data:any)=>
     {
-      console.log(data);
+      // console.log(data);
     alert("Student details Deleted");
     this.getData(); 
     })
   }
 
-   ngOnInit() : void{
-   this.getData();
-   }
+   
 
 
 

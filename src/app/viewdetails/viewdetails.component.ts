d7c136b86dfbe2f310c097a9a3c92b70439c44ad
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-03-06 00:05:08 +0530

    importing ActivatedRoute,service

--- src/app/viewdetails/viewdetails.component.ts
+++ src/app/viewdetails/viewdetails.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-viewdetails',
   templateUrl: './viewdetails.component.html',
   styleUrls: ['./viewdetails.component.css']
 })
-export class ViewdetailsComponent {
+export class ViewdetailsComponent implements OnInit{
+data:any;
+constructor(private route:ActivatedRoute,private userservice:UserService){}
+
+
+ngOnInit(){
+  
+}
 
 }

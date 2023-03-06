5d482d47569d7806b46ea5b1e53b329587cfff57
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-03-06 22:13:24 +0530

    authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,30 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Student } from '../models/student.model';
 import { UserService } from '../user.service';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent  {
+
+  name: any;
+  pass: any;
+  data:any;
+  students: any ;
+  displayMsg:string='';
+  isUserLogged: boolean=false;
 
 
-   constructor(private userservice:UserService){}
+   constructor(private userservice:UserService,private router:Router){
+        this.getData();
+   }
+
    public LoginForm =new FormGroup({
     Name:new FormControl(''),
     Pass:new FormControl()
   });
-   loginSubmitted(){
-             console.log(this.LoginForm.value);
-             this.userservice.loginUser(this.LoginForm.value).subscribe({
-              next:(res)=>{
-                console.log("logged in");
-                
-              }
-             });
-   }
+
+  getData()
+  {
+    this.userservice.getAllStudents().subscribe((data: any)=>
+    {
+      this.data=data;
+       this.students=this.data;
+       console.log(this.students);
+    })
+  }
+
+  //  loginSubmitted(){
+  //            console.log(this.LoginForm.value);
+  //            this.userservice.loginUser(this.LoginForm.value).subscribe({
+  //             next:(res)=>{
+  //               console.log("logged in");
+  //               alert(res.message);
+  //             },
+  //             error:(err)=>{
+  //               console.log("login failed");
+  //               alert(err.error.message);
+  //             }
+
+  //            });
+  //  }
+
+  loginSubmitted(){
+    const user = this.students.find( (x: { name: any; password: any; }) => x.name === this.LoginForm.value.Name && x.password === this.LoginForm.value.Pass);
+ //   console.log(this.students);
+    console.log(user);
+    if (!user) {
+      console.log("logged failed");
+      this.displayMsg='User Not Found';
+    } else{
+      this.displayMsg='User Logged successfully';
+      console.log("login success");
+      this.isUserLogged=true;
+    this.router.navigate(['/viewdetails/'+user.id]);
+    }
+  }
+
+ 
   
   
 }

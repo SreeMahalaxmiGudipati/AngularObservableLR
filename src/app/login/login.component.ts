bb5b40edfb920c62c5a9850e1306bce8066e06bf
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-03-06 18:11:27 +0530

    login authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Student } from '../models/student.model';
 import { UserService } from '../user.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,16 +9,21 @@ import { UserService } from '../user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
- 
-    UserIsValid:boolean=false;
-    http: any;
-username: any;
-pass: any;
+
 
    constructor(private userservice:UserService){}
-
+   public LoginForm =new FormGroup({
+    Name:new FormControl(''),
+    Pass:new FormControl()
+  });
    loginSubmitted(){
-
+             console.log(this.LoginForm.value);
+             this.userservice.loginUser(this.LoginForm.value).subscribe({
+              next:(res)=>{
+                console.log("logged in");
+                
+              }
+             });
    }
   
   

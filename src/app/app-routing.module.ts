f4719a8e4a1feaddcc8f372f14d167961852625d
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-03-14 01:05:59 +0530

    adding authAuard to home page path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ParentComponent } from './parent/parent.component';
 import { ViewdetailsComponent } from './viewdetails/viewdetails.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   // {path:'',component:ParentComponent},
-  { path: '', redirectTo: '/parent', pathMatch: 'full' },
-  {path:'parent',component:ParentComponent},
+  { path: '', redirectTo: '/parent/login', pathMatch: 'full' },
+  {path:'parent',component:ParentComponent,canActivate:[AuthGuard]},
   {path:'',
       children:[
         {path:'parent/login',component:LoginComponent},
